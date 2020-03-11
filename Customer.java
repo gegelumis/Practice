9585c0daf72d4b6582e9a30a9e8a78672674b1ac
gegelumis <matteo.dicunzolo@edu.unito.it>
2020-03-11 11:46:03 +0100

    A funny class which has been abused in literature

---
+++ Customer.java
@@ -0,0 +1,17 @@
+public class Customer{
+	
+	private String name;
+	private String surname;
+	private String otherOriginalThings;
+	
+	public Customer(String name, String surname, String otherOriginalThings){
+		this.name = name;
+		this.surname = surname;
+		this.otherOriginalThings = otherOriginalThings;
+	}
+	
+	public boolean isFunny(){
+		return false;
+	}
+	
+}
